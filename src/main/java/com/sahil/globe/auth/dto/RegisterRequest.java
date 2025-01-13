4a81ef9e5d3d5f9659b82f88a43879b085e6cc30
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-13 09:31:11 +0000

    Create DTOs for request objects

---
+++ src/main/java/com/sahil/globe/auth/dto/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.sahil.globe.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
