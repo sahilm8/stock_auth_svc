54a59c0ec088c13433c8f96847509d569ecc7a87
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-13 23:02:02 +0000

    Add default GET mapping

--- src/main/java/com/sahil/globe/auth/controller/AuthController.java
+++ src/main/java/com/sahil/globe/auth/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.sahil.globe.auth.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,15 @@ import lombok.RequiredArgsConstructor;
 @Validated
 public class AuthController {
     private final AuthService authService;
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok(String.format(
+                "Auth Globe API%n%n" +
+                        "Welcome to the auth endpoint, you can make the following requests:%n" +
+                        "- POST /register%n" +
+                        "- POST /authenticate%n"));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
