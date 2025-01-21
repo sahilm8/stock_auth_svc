f4697f3168ba2086bf4335c629fe8502d76e2a9f
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-21 20:51:53 +0000

    Remove old paths

--- src/main/java/com/sahil/globe/auth/Application.java
+++
@@ -1,9 +0,0 @@
-package com.sahil.globe.auth;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application {
-	public static void main(String[] args) {
-	}
-}
