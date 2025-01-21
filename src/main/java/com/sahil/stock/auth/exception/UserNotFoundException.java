0a273192fbe70bada5da815bbfc046d7f94ce7a4
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-21 20:45:08 +0000

    Update custom exceptions

---
+++ src/main/java/com/sahil/stock/auth/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sahil.stock.auth.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
