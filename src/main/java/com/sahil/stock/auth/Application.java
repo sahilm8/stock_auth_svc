640a8c0d8786db232bdcdb4c4c386691bb60f4d2
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-21 20:44:40 +0000

    Optimise App

---
+++ src/main/java/com/sahil/stock/auth/Application.java
@@ -0,0 +1,9 @@
+package com.sahil.stock.auth;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+	}
+}
