f2272d3031f90ebf545fb19f09ffebc5faa595c0
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-23 19:42:43 +0000

    Setup DB config

---
+++ src/main/java/com/sahil/stock/auth/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.sahil.stock.auth.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseConfig {
+    @Primary
+    @Bean(name = "authDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.auth")
+    public DataSource authDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "portfolioDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.portfolio")
+    public DataSource portfolioDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
