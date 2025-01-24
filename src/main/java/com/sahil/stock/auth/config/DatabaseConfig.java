bc0d4e7b00a4588cac67eaf56982351757d84a97
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-24 15:37:48 +0000

    Update DBConfig

--- src/main/java/com/sahil/stock/auth/config/DatabaseConfig.java
+++ src/main/java/com/sahil/stock/auth/config/DatabaseConfig.java
@@ -2,11 +2,14 @@ package com.sahil.stock.auth.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,9 +23,30 @@ public class DatabaseConfig {
         return DataSourceBuilder.create().build();
     }
 
+    @Primary
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("authDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.sahil.stock.auth.model")
+                .build();
+    }
+
     @Bean(name = "portfolioDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.portfolio")
     public DataSource portfolioDataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "portfolioEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean portfolioEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("portfolioDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.sahil.stock.auth.model")
+                .build();
     }
 }
